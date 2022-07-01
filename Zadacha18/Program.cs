663b815390d65e8435dc4e0cff052671dc8b384f
Maxim <m.krishnevskiy@kmg.kz>
2022-07-01 19:01:35 +0600

    Zadacha17, 18, 19

---
+++ Zadacha18/Program.cs
@@ -0,0 +1,62 @@
+﻿int n;
+n = Convert.ToInt32(Console.ReadLine());
+if (n > 0 && n < 5)
+{
+
+    if (n == 1)
+    {
+        Console.WriteLine("x = ( 0, +~)");
+        Console.WriteLine("y = ( 0, +~)");
+    }
+
+    if (n == 2)
+    {   
+        Console.WriteLine("x = ( -~, 0)");
+        Console.WriteLine("y = ( 0, +~)");
+    }
+
+    if (n == 3)
+    {
+        Console.WriteLine("x = ( -~, 0)");
+        Console.WriteLine("y = ( -~, 0)");
+    }
+
+    if (n == 4)
+    {
+        Console.WriteLine("x = ( 0, +~)");
+        Console.WriteLine("y = ( -~, 0)");
+    }
+}
+else
+{
+    Console.WriteLine("Такой четверти не существует! Ты врун!");
+}
+
+
+
+// int x, y;
+// x = Convert.ToInt32(Console.ReadLine());
+// y = Convert.ToInt32(Console.ReadLine());
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("x = ( 0, +~)");
+//     Console.WriteLine("y = ( 0, +~)");
+// }
+
+// if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("x = ( -~, 0)");
+//     Console.WriteLine("y = ( 0, +~)");
+// }
+
+// if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("x = ( -~, 0)");
+//     Console.WriteLine("y = ( -~, 0)");
+// }
+
+// if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("x = ( 0, +~)");
+//     Console.WriteLine("y = ( -~, 0)");
+// }
