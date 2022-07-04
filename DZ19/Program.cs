86779676c166422cdd77efda7bf5d8132e150ed9
Maxim <m.krishnevskiy@kmg.kz>
2022-07-04 17:18:20 +0600

    Zadacha24_

--- DZ19/Program.cs
+++ DZ19/Program.cs
@@ -1,11 +1,27 @@
-﻿int n;
-n = Convert.ToInt32(Console.ReadLine());
-//n = n % 100/10;//1000; // 1000; // - n % 100000 / 10000;
-if ((n % 100000/10000 == n % 10 && n % 10000/1000==n % 100/10))
+﻿// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// //n = n % 100/10;//1000; // 1000; // - n % 100000 / 10000;
+// if ((n % 100000/10000 == n % 10 && n % 10000/1000==n % 100/10))
+// {
+//     Console.WriteLine("Число является палиндромом ");
+// }
+// else
+// {
+//     Console.WriteLine("Число не является палиндромом ");
+// }
+
+int n = Convert.ToInt32(Console.ReadLine());
+int n1, n2, n4, n5;
+n1 = n / 10000;
+n2 = (n % 10000) / 1000;
+n4 = (n % 100) / 10;
+n5 = n % 10;
+if (n1 == n5 && n2 == n4)
 {
-    Console.WriteLine("Число является палиндромом ");
+    Console.WriteLine("Является палендромом");
 }
 else
 {
-    Console.WriteLine("Число не является палиндромом ");
+    Console.WriteLine("Не является палендромом");
 }
+
